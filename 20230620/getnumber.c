914f5e1168ba48e1d1c03c1b4cc06581ff6fbc3a
skuviz21 <skuviz21@htl-kaindorf.at>
2023-06-20 14:46:28 +0200

    zahleneingabe

--- 20230620/getnumber.c
+++ 20230620/getnumber.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
+int getIntegerNumber ( char userprompt [], int ll, int ul )
+{
+  int zahl = 0;
+
+  do
+  {
+    printf("%s", userprompt);
+    scanf("%d", &zahl);
+  } while (zahl < ll || zahl > ul);
+
+  return zahl;
+}
+
 int main (void)
 {
   
